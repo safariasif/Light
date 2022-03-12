5f758149e07321637f9c72cf224f79a2dd4ad8d8
SafariAsif <mohammadasifsafari1997@gmail.com>
2022-03-12 13:55:05 +0430

    created banner section

---
+++ components/Banner/banner.js
@@ -0,0 +1,17 @@
+const Banner=()=>{
+    return(
+        <div className="container-fluid banner">
+            <div className="row">
+                <div className="title">
+                    <h1 className="">چراغ</h1>
+                </div>
+                <div className="description">
+                    <span>
+                        با چراغ برنامه نویسی به لذت بخش ترین تفریح برایت مبدل میشود
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Banner
