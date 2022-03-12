18fedd7d62b15da23c21c5742b2b2a1f67d62afb
SafariAsif <mohammadasifsafari1997@gmail.com>
2022-03-12 21:14:18 +0430

    updated navbar

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -1,30 +1,57 @@
-import NavLink from 'next/link'
+import Link from 'next/Link'
 const Navbar=()=>{
-    const navbar=['خانه' ,'دوره های آموزشی' ,'جدیدترین دوره ها' ,'پست ها' ,'کانال تلگرام' ,'پشنهاد' ,'درباره ما']
-    return(
-        <div>
-            <div className="container-fluid m-0 p-0 navigation-bar">
-                <div className="m-0 p-0 row flex justify-between  rounded py-4 px-4 text-center">
-                    <div className="col-4  m-0 p-0">
-                        <button className='regester btn rounded text-blue-800 font-bold px-5 py-2'>ثبت نام / ورورد</button>
-                    </div>
-                    <div className="col-lg-8 m-0 p-0 lg:block hidden">
-                        <ul className='flex'>
-                            {
-                                navbar.reverse().map((ele,index)=>{
-                                   return(
-                                    <li  className='nav-item px-4 py-2 rounded'>
-                                        <NavLink to='/' href='/' key={index}>{ele}</NavLink>
-                                    </li>
-                                   )
-                                })
-                            }
-                        </ul>
-                    </div>
-                </div>
+    return (
+      <div>
+        <div className="container-fluid m-0 p-0 navigation-bar">
+          <div className="m-0 p-0 row flex justify-between  rounded py-4 px-4 text-center">
+            <div className="col-4  m-0 p-0">
+              <button className="regester btn rounded text-blue-800 font-bold px-5 py-2">
+                ثبت نام / ورورد
+              </button>
             </div>
+            <div className="col-lg-8 m-0 p-0 lg:block hidden">
+              <ul className="flex">
+                <li className="items">
+                  <Link href="/about">
+                    <a>درباره ما</a>
+                  </Link>
+                </li>
+                <li className="items">
+                  <Link href="/suggest">
+                    <a>پشنهادات</a>
+                  </Link>
+                </li>
+                <li className="items">
+                  <Link href="/telegram">
+                    <a>کانال تلگرام</a>
+                  </Link>
+                </li>
+                <li className="items">
+                  <Link href="/posts">
+                    <a>پست ها</a>
+                  </Link>
+                </li>
+                <li className="items">
+                  <Link href="/new">
+                    <a>جدید ترن ها</a>
+                  </Link>
+                </li>
+                <li className="items">
+                  <Link href="/Course">
+                    <a>دوره های آموزشی</a>
+                  </Link>
+                </li>
+                <li className="items">
+                  <Link href="/">
+                    <a>خانه</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Navbar;
