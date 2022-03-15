3bc678c6e422c4b02818d6380c36edde5df0e17f
SafariAsif <mohammadasifsafari1997@gmail.com>
2022-03-15 19:21:26 +0430

    updated navbar

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -49,6 +49,21 @@ const Navbar = () => {
                 <a>درباره ما</a>
               </Link>
             </li>
+            <span className="regester btn cursor-pointer lg:hidden block grid grid-cols-2 rounded text-center text-blue-800 font-bold px-5  w-full mt-2 justify-around">
+              <span className='bg-green-300 py-2 mx-2 rounded-md'>
+                <Link href="/login">
+                  <a className="login">ورود</a>
+                </Link>
+                <Link href="/regester">
+                  <a className="hover:text-gray-500 "> / ثبت نام</a>
+                </Link>
+              </span>
+              <input
+                type="text"
+                className="border border-2 px-2 rounded-md"
+                placeholder="چه میخواهی یاد بیگیری؟"
+              ></input>
+            </span>
           </ul>
           <div className=" m-0 p-0 flex justify-between items-center">
             <span className="">
@@ -71,12 +86,17 @@ const Navbar = () => {
               )}
             </span>
 
-            <span className="regester btn cursor-pointer rounded text-center text-blue-800 font-bold px-5 py-2">
+            <span className="regester btn cursor-pointer hidden lg:block rounded text-center text-blue-800 font-bold px-5 py-2">
               <Link href="/login">
                 <a className="login">ورود</a>
               </Link>
               <Link href="/regester">
                 <a className="hover:text-gray-500 "> / ثبت نام</a>
+              </Link>
+            </span>
+            <span className="regester lg:hidden block btn cursor-pointer rounded text-center text-blue-800 font-bold px-5 py-2">
+              <Link href="/login">
+                <a className="login">چراغ</a>
               </Link>
             </span>
           </div>
