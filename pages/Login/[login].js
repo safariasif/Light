85b4574a72674979fb63d2e99e77b2fed56cfa8b
SafariAsif <mohammadasifsafari1997@gmail.com>
2022-03-17 21:46:38 +0430

    changed functional to class component

--- pages/Login/[login].js
+++ pages/Login/[login].js
@@ -2,27 +2,39 @@ import Link from 'next/link'
 import Navbar from '../../components/Navbar/navbar'
 import Meta from '../../components/Meta/Meta'
 import Input from '../Regester/inputRegester';
-const Login=()=>{
+import { Component } from 'react';
+class Login extends Component {
+  render() {
     return (
       <div>
         <Meta title={'ورود'}></Meta>
         <Navbar />
         <div className="w-full  mt-12 grid md:grid-cols-2 justify-center items-center space-x-5">
           <div className="text-start md:px-16 px-5   text-lg ">
-            <form onSubmit={handelSubmit}>
+            <form onSubmit={this.HandelSubmit}>
               <div className="text-center font-bold text-2xl">
                 <h1>ورود</h1>
               </div>
-              <Input label={"ایمیل"} name={"email"} placeholder={"ایمیل تان را بنویسید"} type={"email"}/>
-              <Input label={"رمز عبور"} name={"password"} placeholder={"رمز عبور تان را بنویسید"} type={"password"}/>
+              <Input
+                label={'ایمیل'}
+                name={'email'}
+                placeholder={'ایمیل تان را بنویسید'}
+                type={'email'}
+              />
+              <Input
+                label={'رمز عبور'}
+                name={'password'}
+                placeholder={'رمز عبور تان را بنویسید'}
+                type={'password'}
+              />
               <div className="my-2">
                 <label className="text-gray-600 px-2 ">به یاد داشتنن:</label>
                 <input className="outline-none" type="checkbox"></input>
               </div>
               <div className="my-4">
-                  <button className="block w-full py-2 bg-blue-800 text-white hover:bg-green-300 transition-all hover:text-gray-800 rounded-md">
-                    ورود
-                  </button>
+                <button className="block w-full py-2 bg-blue-800 text-white hover:bg-green-300 transition-all hover:text-gray-800 rounded-md">
+                  ورود
+                </button>
               </div>
               <div className="my-4 font-mono text-md  grid md:grid-cols-2 sm:grid-cols-2 justify-between px-3">
                 <div>
@@ -48,10 +60,10 @@ const Login=()=>{
         </div>
       </div>
     );
-   function handelSubmit(e){
-        e.preventDefault();
-        console.log('sunmit');
-    }
+  }
+  HandelSubmit=(e)=>{
+    e.preventDefault();
+  };
 }
 
 export default Login;
