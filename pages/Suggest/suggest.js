64df6ac3aadfdca794740d20a3e5bdbcbe308e25
SafariAsif <mohammadasifsafari1997@gmail.com>
2022-03-19 10:59:21 +0430

    created  post and suggest section

---
+++ pages/Suggest/suggest.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import Navbar from '../../components/Navbar/navbar'
+class Suggest extends Component{
+render() {
+    return (
+         <div>
+             <Navbar/>
+         </div>
+    );
+}
+}
+
+export default Suggest;
