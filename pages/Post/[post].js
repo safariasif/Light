a990507279fccaa3b1eca85b4b1509b7226806b1
SafariAsif <mohammadasifsafari1997@gmail.com>
2022-03-19 10:59:58 +0430

    updated post file

---
+++ pages/Post/[post].js
@@ -0,0 +1,118 @@
+import { Component } from "react";
+import Navbar from "../../components/Navbar/navbar";
+
+class Post extends Component{
+        render() {
+            return (
+              <div>
+                <Navbar />
+                <div className="gird">
+                  <div className="item-start m-6 grid grid-cols-2 gap-3">
+                    <div className="">
+                      <div className="flex">
+                        <img
+                          src="/profile/profile (2).jpg"
+                          className="w-20 h-20 rounded-full"
+                        ></img>
+                        <div className="posttitle mt-5">
+                          <h1 className="m-3 text-lg">
+                            چطور پروژه های شکیل و مقبول دیزان کینیم؟
+                          </h1>
+                        </div>
+                      </div>
+                      <div className="postdescription">
+                        <span className="text-md m-4">
+                          اینجانب محمد صفری کارشناس و تحلیلگر در حوزه ارزهای
+                          رمزنگاری و سهام های بین المللی به شما متعهد میشوم که
+                          در پایان دوره موج سواری بازار مالی و دوپینگ مالی ، با
+                          حضور در این ۲ دوره آنلاین و تمرین های هفتگی و شرکت در
+                          کلاس های رفع اشکال در دوره دوپینگ مالی، به راحتی با
+                          کسب دانش و رعایت تمامی موارد گفته شده معامله گری در
+                          بازار های مالی را بصورت حرفه ای یادگرفته و به درآمد
+                          های مورد دلخواه خود برسید. من به شما قول میدهم تا
+                          زمانی که در بازار
+                        </span>
+                      </div>
+                    </div>
+                    <div>
+                      <img
+                        src="/Post/post1.png"
+                        className="border border-2 rounded-lg px-3"
+                      ></img>
+                    </div>
+                  </div>
+                  <div className="item-start m-6 grid grid-cols-2 gap-3">
+                    <div className="">
+                      <div className="flex">
+                        <img
+                          src="/profile/profile (2).jpg"
+                          className="w-20 h-20 rounded-full"
+                        ></img>
+                        <div className="posttitle mt-5">
+                          <h1 className="m-3 text-lg">
+                            چطور پروژه های شکیل و مقبول دیزان کینیم؟
+                          </h1>
+                        </div>
+                      </div>
+                      <div className="postdescription">
+                        <span className="text-md m-4">
+                          اینجانب محمد صفری کارشناس و تحلیلگر در حوزه ارزهای
+                          رمزنگاری و سهام های بین المللی به شما متعهد میشوم که
+                          در پایان دوره موج سواری بازار مالی و دوپینگ مالی ، با
+                          حضور در این ۲ دوره آنلاین و تمرین های هفتگی و شرکت در
+                          کلاس های رفع اشکال در دوره دوپینگ مالی، به راحتی با
+                          کسب دانش و رعایت تمامی موارد گفته شده معامله گری در
+                          بازار های مالی را بصورت حرفه ای یادگرفته و به درآمد
+                          های مورد دلخواه خود برسید. من به شما قول میدهم تا
+                          زمانی که در بازار
+                        </span>
+                      </div>
+                    </div>
+                    <div>
+                      <img
+                        src="/Post/post1.png"
+                        className="border border-2 rounded-lg px-3"
+                      ></img>
+                    </div>
+                  </div>
+                  <div className="item-start m-6 grid grid-cols-2 gap-3">
+                    <div className="">
+                      <div className="flex">
+                        <img
+                          src="/profile/profile (2).jpg"
+                          className="w-20 h-20 rounded-full"
+                        ></img>
+                        <div className="posttitle mt-5">
+                          <h1 className="m-3 text-lg">
+                            چطور پروژه های شکیل و مقبول دیزان کینیم؟
+                          </h1>
+                        </div>
+                      </div>
+                      <div className="postdescription">
+                        <span className="text-md m-4">
+                          اینجانب محمد صفری کارشناس و تحلیلگر در حوزه ارزهای
+                          رمزنگاری و سهام های بین المللی به شما متعهد میشوم که
+                          در پایان دوره موج سواری بازار مالی و دوپینگ مالی ، با
+                          حضور در این ۲ دوره آنلاین و تمرین های هفتگی و شرکت در
+                          کلاس های رفع اشکال در دوره دوپینگ مالی، به راحتی با
+                          کسب دانش و رعایت تمامی موارد گفته شده معامله گری در
+                          بازار های مالی را بصورت حرفه ای یادگرفته و به درآمد
+                          های مورد دلخواه خود برسید. من به شما قول میدهم تا
+                          زمانی که در بازار
+                        </span>
+                      </div>
+                    </div>
+                    <div>
+                      <img
+                        src="/Post/post1.png"
+                        className="border border-2 rounded-lg px-3"
+                      ></img>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+        }
+}
+
+export default Post;
