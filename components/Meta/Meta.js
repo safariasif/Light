aece7bd4b935f8df777b8846edbea2def2d10de8
SafariAsif <mohammadasifsafari1997@gmail.com>
2022-03-27 22:36:11 +0430

    add image in the meta page

--- components/Meta/Meta.js
+++ components/Meta/Meta.js
@@ -6,6 +6,7 @@ const Meta = ({ title, icon }) => {
       <Head>
         <meta lang="en"></meta>
         <title>{title}</title>
+        <link rel="icon" className="h-12 w-12" href="/logo/lamp.jpg"></link>
         <link href={icon}></link>
       </Head>
     </div>
