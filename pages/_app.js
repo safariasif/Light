7207168b04fe96860a8dd01bdf883a60c24327ad
SafariAsif <mohammadasifsafari1997@gmail.com>
2022-03-29 06:37:06 +0430

    installed react-loading-skletone and added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'; 
 import { config } from '@fortawesome/fontawesome-svg-core';
+import 'react-loading-skeleton/dist/skeleton.css'
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
